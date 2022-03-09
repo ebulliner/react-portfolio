6a2586609365d0be0b85ec9a7638040389776d5c
Evan Bulliner <evan_bulliner@yahoo.com>
2022-03-08 19:41:12 -0600

    created, styled, and added an image to the section.js

---
+++ src/components/Section.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import evan from './assets/man.jpeg'
+import './styles/Section.css'
+
+
+const Section = () => {
+    return (
+        <section className='main-section'>
+            <div className='container'>
+
+
+                <div className="texts">
+                    <h1>Hi, My Name is</h1>
+                    <h1 className='h1-big'>Evan Bulliner</h1>
+                    <p>I am a Software Engineer/Full Stack Developer who graduated a coding bootcamp at Northwestern.</p>
+                    <button className='text-button'>My Work</button>
+                </div>
+
+                <div className="image">
+                    <img src={evan} alt="" />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Section
